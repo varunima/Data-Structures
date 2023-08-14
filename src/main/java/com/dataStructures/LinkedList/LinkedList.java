cd9a390c5a93aaa818f2a7ac8b2692e88f37d016
Varunima <baghelvarunima@gmail.com>
2023-08-15 00:19:21 +0530

    LL prepend

--- src/main/java/com/dataStructures/LinkedList/LinkedList.java
+++ src/main/java/com/dataStructures/LinkedList/LinkedList.java
@@ -29,14 +29,15 @@ public class LinkedList {
 			temp = temp.next;
 		}
 	}
-	
+
 	public void getHead() {
 		System.out.println("Head:" + head.value);
 	}
-	
+
 	public void getTail() {
 		System.out.println("Tail:" + tail.value);
 	}
+
 	public void getLength() {
 		System.out.print("Length:" + length);
 	}
@@ -54,23 +55,36 @@ public class LinkedList {
 		}
 		length++;
 	}
+
 	public Node removeLast() {
-		Node temp=head;
-		Node pre=head;
-		if(length==0)
+		Node temp = head;
+		Node pre = head;
+		if (length == 0)
 			return null;
-		while(temp.next!=null) {
-			pre=temp;
-			temp=temp.next;
+		while (temp.next != null) {
+			pre = temp;
+			temp = temp.next;
 		}
-		tail=pre;
-		tail.next=null;
+		tail = pre;
+		tail.next = null;
 		length--;
-		if(length==0) {
-			head=null;
-			tail=null;
+		if (length == 0) {
+			head = null;
+			tail = null;
 		}
 		return temp;
 	}
-	
+
+	public void prepend(int value) {
+		Node newnode = new Node(value);
+		if (length == 0) {
+			head = newnode;
+			tail = newnode;
+		} else {
+			newnode.next = head;
+			head = newnode;
+		}
+		length++;
+	}
+
 }
